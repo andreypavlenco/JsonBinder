80babfd504d6b85bad515d9e69431b683a229f69
andreypavlenco <and.pavel.java@gmail.com>
2024-12-05 00:53:23 +0200

    feat: update category.service.ts

--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -1,10 +1,8 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
-import { Products } from '@prisma/client';
 import { CategoriesRepository } from 'src/repositories/repository/categories.repository';
 import { CreateCategoriesDto } from './dto/create-categories-dto';
 import { extractUniqueCategories } from './utils/extract-categories';
 import { ReadFileService } from 'src/fs-module/fs.read/fs.read.service';
-import { CreateProductsDto } from 'src/products/dto/create-products-dto';
 
 @Injectable()
 export class CategoriesService {
@@ -13,53 +11,54 @@ export class CategoriesService {
     private readonly fileReadService: ReadFileService,
   ) {}
 
-  async createCategoriesFromFile(): Promise<void> {
+  async createCategoriesFromFile(): Promise<{ count: number }> {
     try {
-      const products: CreateProductsDto[] =
-        await this.fileReadService.readFile();
-      const uniqueCategory = extractUniqueCategories(products);
-      await this.createManyFromList(uniqueCategory);
+      const products = await this.fileReadService.readFile();
+      const categoryNames = extractUniqueCategories(products);
+      return await this.addUniqueCategories(categoryNames);
     } catch (error) {
-      throw new BadRequestException('Error while creating categories', error);
-    }
-  }
-
-  private async createManyFromList(
-    categories: string[],
-  ): Promise<{ count: number } | true> {
-    try {
-      const uniqueCategory: CreateCategoriesDto[] = categories.map(
-        (category) => ({
-          name: category,
-          createdAt: new Date(),
-        }),
+      throw new BadRequestException(
+        'Failed to create categories from file',
+        error,
       );
-
-      return await this.checkAndAddUniquecategories(uniqueCategory);
-    } catch (error) {
-      console.error('Error while parsing categories:', error);
-      throw new BadRequestException('Failed to parse categories');
     }
   }
 
-  private async checkAndAddUniquecategories(categories: CreateCategoriesDto[]) {
-    try {
-      const existingCategories = await this.findAll();
+  private async addUniqueCategories(
+    categoryNames: string[],
+  ): Promise<{ count: number }> {
+    const categoriesDto = this.mapToCategoryDtos(categoryNames);
+    const existingCategories = await this.getAllCategories();
 
-      const uniqueCategories = categories.filter((category) => {
-        return !existingCategories.some(
-          (existingCategories) => existingCategories.name === category.name,
-        );
-      });
+    const newCategories = this.filterUniqueCategories(
+      existingCategories,
+      categoriesDto,
+    );
 
-      if (uniqueCategories.length > 0) {
-        return await this.saveCategories(uniqueCategories);
-      } else {
-        return true;
-      }
-    } catch (error) {
-      throw new BadRequestException('Error creating many brands', error);
+    if (newCategories.length > 0) {
+      return await this.saveCategories(newCategories);
+    } else {
+      return { count: 0 };
     }
+  }
+
+  private mapToCategoryDtos(categoryNames: string[]): CreateCategoriesDto[] {
+    return categoryNames.map((name) => ({
+      name,
+      createdAt: new Date(),
+    }));
+  }
+
+  private filterUniqueCategories(
+    existingCategories: CreateCategoriesDto[],
+    newCategories: CreateCategoriesDto[],
+  ): CreateCategoriesDto[] {
+    return newCategories.filter(
+      (newCategory) =>
+        !existingCategories.some(
+          (existingCategory) => existingCategory.name === newCategory.name,
+        ),
+    );
   }
 
   private async saveCategories(
@@ -68,12 +67,11 @@ export class CategoriesService {
     try {
       return await this.categoriesRepository.createMany(categories);
     } catch (error) {
-      console.error('Error while saving categories:', error);
-      throw new BadRequestException('Failed to save categories');
+      throw new BadRequestException('Failed to save categories', error);
     }
   }
 
-  async findAll() {
+  private async getAllCategories(): Promise<CreateCategoriesDto[]> {
     try {
       return await this.categoriesRepository.findAll();
     } catch (error) {
