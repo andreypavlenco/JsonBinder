498462110eebf88acbbcfe02472ba4b35b5d2ff9
andreypavlenco <and.pavel.java@gmail.com>
2024-12-06 04:44:21 +0200

    feat:add brands.import.from.jsom.module and  brands.import.from.jsom.service

--- src/brands/brands.module.ts
+++ src/brands/brands.module.ts
@@ -1,15 +1,9 @@
-import { forwardRef, Module } from '@nestjs/common';
-import { ReadFileModule } from 'src/json-file-service/json-read/json.read.module';
+import { Module } from '@nestjs/common';
 import { BrandsService } from './brands.service';
 import { RepositoryModule } from 'src/repositories/repository/repository.module';
-import { WriteFileModule } from 'src/json-file-service/json-write/json.write.module';
 
 @Module({
-  imports: [
-    ReadFileModule,
-    RepositoryModule,
-    forwardRef(() => WriteFileModule),
-  ],
+  imports: [RepositoryModule],
   providers: [BrandsService],
   exports: [BrandsService],
 })
