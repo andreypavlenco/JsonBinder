f0e7b82cadcc98bc66c9a9d9050f4843555ddd08
andreypavlenco <and.pavel.java@gmail.com>
2024-12-11 23:00:46 +0200

    feat:add cache.service.ts

---
+++ src/cache/cache.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@nestjs/common';
+import { CreateProductsDto } from 'src/products/dto/create-products-dto';
+import { RedisService } from 'src/redis/redis.service';
+
+@Injectable()
+export class CacheService {
+  constructor(private readonly redisService: RedisService) {}
+
+  async cacheData(key: string, value: CreateProductsDto[]): Promise<void> {
+    try {
+      await this.redisService.setReadFileJson(key, JSON.stringify(value));
+    } catch (error) {
+      console.error(`Failed to set cache for key ${key}:`, error);
+    }
+  }
+
+  async retrieveFromCache(key: string): Promise<CreateProductsDto[] | null> {
+    try {
+      const cachedData = await this.redisService.getFileJson(key);
+      return cachedData ? JSON.parse(cachedData) : null;
+    } catch (error) {
+      return null;
+    }
+  }
+}
