f57fbad95f79b8a056994bebdc8b8f0b476e6ecc
andreypavlenco <and.pavel.java@gmail.com>
2024-12-17 19:06:47 +0200

    feat:add categories.controller.ts

--- src/brands/brands.module.ts
+++ src/brands/brands.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { BrandsService } from './brands.service';
 import { RepositoryModule } from 'src/repositories/repository/repository.module';
+import { BrandsController } from './brands.controller';
 
 @Module({
   imports: [RepositoryModule],
+  controllers: [BrandsController],
   providers: [BrandsService],
   exports: [BrandsService],
 })
--- src/categories/categories.module.ts
+++ src/categories/categories.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CategoriesService } from './categories.service';
 import { RepositoryModule } from 'src/repositories/repository/repository.module';
+import { CategoriesController } from './categories.controller';
 
 @Module({
   imports: [RepositoryModule],
+  controllers: [CategoriesController],
   providers: [CategoriesService],
   exports: [CategoriesService],
 })
