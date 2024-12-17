642a52a4055b314ebab94a69535e807f6ea0a12e
andreypavlenco <and.pavel.java@gmail.com>
2024-12-17 18:52:00 +0200

    feat:update brands.controller, brands.repository.ts, add update-brands-dto.ts

--- src/repositories/repository/brands.repository.ts
+++ src/repositories/repository/brands.repository.ts
@@ -3,6 +3,7 @@ import { Brands } from '@prisma/client';
 import { PrismaService } from 'prisma/prisma.service';
 import { IBrandsRepository } from '../interfaces/brands-repository.interface';
 import { CreateBrandsDto } from 'src/brands/dto/create-brands-dto';
+import { UpdateBrandsDto } from 'src/brands/dto/update-brands-dto';
 
 @Injectable()
 export class BrandsRepository implements IBrandsRepository {
@@ -19,5 +20,35 @@ export class BrandsRepository implements IBrandsRepository {
 
   async findAll(): Promise<Brands[]> {
     return this.prisma.brands.findMany();
+  }
+
+  async findOne(idBrands: string): Promise<Brands> {
+    return await this.prisma.brands.findUnique({
+      where: {
+        id: idBrands,
+      },
+    });
+  }
+
+  async delete(idBrands: string): Promise<{ name: string }> {
+    return await this.prisma.brands.delete({
+      where: {
+        id: idBrands,
+      },
+      select: {
+        name: true,
+      },
+    });
+  }
+
+  async update(idBrands: string, dto: UpdateBrandsDto): Promise<Brands> {
+    return await this.prisma.brands.update({
+      where: {
+        id: idBrands,
+      },
+      data: {
+        ...dto,
+      },
+    });
   }
 }
