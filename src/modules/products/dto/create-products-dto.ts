1a4f6055e14000ad1227af5e4bf797a1df17d1db
andreypavlenco <and.pavel.java@gmail.com>
2025-01-20 20:30:06 +0200

    fix: update create-products-dto.ts

--- src/modules/products/dto/create-products-dto.ts
+++ src/modules/products/dto/create-products-dto.ts
@@ -1,33 +1,61 @@
-import { IsArray, IsNumber, IsString } from 'class-validator';
+import { MiddlewareBuilder } from '@nestjs/core';
+import {
+  ArrayNotEmpty,
+  IsArray,
+  IsDecimal,
+  IsNotEmpty,
+  IsNumber,
+  IsString,
+  IsUrl,
+  isURL,
+  IsUUID,
+  Length,
+  Max,
+  Min,
+} from 'class-validator';
+
+// @TODO check:
+// length (empty sting is useless) (DB throw the error if big size)
+// numbers after point for decimal
+// @IsUrl if img must be url
+// uuid if foreign key
 
 export class CreateProductsDto {
-  @IsString()
+  @IsUUID()
   categoryId: string;
 
   @IsString()
+  @IsNotEmpty()
+  @Length(40)
   title: string;
 
-  @IsNumber()
+  @IsDecimal()
+  @Min(1)
   price: number;
 
   @IsArray()
-  @IsString({ each: true })
+  @IsUrl({}, { each: true })
+  @IsNotEmpty({ each: true })
   img: string[];
 
   @IsString()
   brand: string;
 
-  @IsString()
+  @IsUUID()
   brandId: string;
 
   @IsArray()
-  @IsString({ each: true })
+  @IsUrl()
   description: string[];
 
   @IsNumber()
+  @Min(0)
+  @Max(5)
   rating: number;
 
   @IsArray()
+  @ArrayNotEmpty()
   @IsString({ each: true })
+  @IsNotEmpty({ each: true })
   characteristics: string[];
 }
